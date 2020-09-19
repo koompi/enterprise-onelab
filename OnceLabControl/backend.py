2d70a36fca53e2d0732287c2b3d590ab9466ad7a
reegsie <45556220+reegsie@users.noreply.github.com>
2020-09-19 17:38:27 +0700

    Update backend.py

--- OnceLabControl/backend.py
+++ OnceLabControl/backend.py
@@ -3,10 +3,10 @@ import os
 #All previous code is related to the UI the following code will be a means of comunicating with the host machine.
 
 def ssh_initInteract():
-    os.system("ssh hangsia@192.168.1.79")
+    os.system("")
 
 def ssh_initPuasePlay():
-    print("Hello I am connected successfully")
+    print("")
 
 def ssh_initStopStart():
-    print("Hello I am connected successfully")
+    print("")
